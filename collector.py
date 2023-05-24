d92e7bceffa80d279a1346befe75e365c2036304
alescrocaro <Alexandre A. Scrocaro Junior>
2023-05-24 20:09:44 -0300

    update collector

--- collector.py
+++ collector.py
@@ -5,6 +5,7 @@ import sys
 
 class Collector:
     def __init__(self):
+        print("initing collector...")
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
         self.channel = self.connection.channel()
         self.tweets = []
@@ -12,17 +13,23 @@ class Collector:
 
 
     def preprocess_data(self):
-        with open("./tweets.csv", "r") as file:
-            reader = csv.reader(file)
-            
-            # Skip header row
-            next(reader)
+        print("preprocessing tweets...")
+        try:
+            with open("./tweets.csv", "r") as file:
+                reader = csv.reader(file)
+                
+                # Skip header row
+                next(reader)
 
-            for tweet in reader:
-                self.tweets.append({ "topic": tweet[6][1:].lower(), "text": tweet[13], "user_name": tweet[56] })
+                for tweet in reader:
+                    self.tweets.append({ "topic": tweet[6][1:].lower(), "text": tweet[13], "user_name": tweet[56] })
+        
+        except Exception as e:
+            print(f"Error while preprocessing tweets: {e}" )
 
 
     def handle_queue(self):
+        print("publishing all tweets in queue...")
         self.channel.queue_declare(queue="tweets")
         stringified_tweets = json.dumps(self.tweets)
         self.channel.basic_publish(exchange="", routing_key="tweets", body=stringified_tweets)
